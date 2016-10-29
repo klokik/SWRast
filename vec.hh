5deff5d17a59af042e1e44bf449b3ceae60eb8ad
Cadance <0xb000@gmail.com>
2016-10-30 01:24:41 +0300

    add basic matrix math

--- vec.hh
+++ vec.hh
@@ -194,3 +194,52 @@ static vec4<T> operator*(const T _c, const vec4<T> _a) {
   return _a*_c;
 }
 
+template<class T>
+class Mat3x3
+{
+  public: Mat3x3(std::array<T, 9> _vals) {
+    memcpy(val, &_vals[0], sizeof(T) * 9);
+  }
+
+  public: Mat3x3(const vec3<T> _a, const vec3<T> _b, const vec3<T> _c) {
+    val[0][0] = _a.x;
+    val[0][1] = _b.x;
+    val[0][2] = _c.x;
+    val[1][0] = _a.y;
+    val[1][1] = _b.y;
+    val[1][2] = _c.y;
+    val[2][0] = _a.z;
+    val[2][1] = _b.z;
+    val[2][2] = _c.z;
+  }
+
+  public: constexpr T det() const
+  {
+    return (val[0][0]*val[1][1]*val[2][2]+
+            val[0][1]*val[1][2]*val[2][0]+
+            val[1][0]*val[2][1]*val[0][2])-
+           (val[0][2]*val[1][1]*val[2][0]+
+            val[1][0]*val[0][1]*val[2][2]+
+            val[2][1]*val[1][2]*val[0][0]);
+  }
+
+  public: constexpr vec3<T> col(const int _col) const {
+    return {val[0][_col], val[1][_col], val[2][_col]};
+  }
+
+  public: static vec3<T> linSolve(const Mat3x3<T> &_mat, const vec3<T> _b)
+  {
+    auto d1 = Mat3x3<T>(_b, _mat.col(1), _mat.col(2)).det();
+    auto d2 = Mat3x3<T>(_mat.col(0), _b, _mat.col(2)).det();
+    auto d3 = Mat3x3<T>(_mat.col(0), _mat.col(1), _b).det();
+    auto D = _mat.det();
+
+    return {d1/D, d2/D, d3/D};
+  }
+
+  public: static vec3<T> linSolve(const Mat3x3 &_mat, const vec3<T> _b, int _it);
+
+  T val[3][3];
+};
+
+using Mat3f3 = Mat3x3<float>;
